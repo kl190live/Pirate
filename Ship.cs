6e8d9cc8fa73018a3bedff2dec9890a7d155dfc1
Petrik Diak <diak@petrik.hu>
2023-10-05 10:50:52 +0200

    In the ship variable continued doign things that almost work

--- Ship.cs
+++ Ship.cs
@@ -8,15 +8,35 @@ namespace Pirate
 {
 	internal class Ship 
 	{
+		private string captian;
+		private string crew;
 		private List<Pirate> pirate;
+		private static Random random = new Random();
 
-		public Ship(List<Pirate> pirate)
+		public Ship(string captian, string crew, List<Pirate> pirate)
 		{
-			this.pirate = pirate;
+			this.Captian = captian;
+			this.Crew = crew;
+			this.Pirate = pirate;
 		}
 
+		public string Captian { get => captian; set => captian = value; }
+		public string Crew { get => crew; set => crew = value; }
+		internal List<Pirate> Pirate { get => pirate; set => pirate = value; }
+
+		public string fillship()
+		{
+			int board=random.Next(114);
+			for (int i = 0; i < board; i++)
+			{
 
 
+
+
+			}
+			
+			
+		}
 
 
 
